8d93c1705179ef3910844d7fc7cb1874412c2c44
Sol Beatriz Rico Cadavid <50256067+soricoc@users.noreply.github.com>
2019-11-27 21:25:40 -0500

    q01

--- 01-hadoop-50/q01-10/mapper.py
+++ 01-hadoop-50/q01-10/mapper.py
@@ -1,4 +1,7 @@
+#! /usr/bin/env python
+
 import sys
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+if __name__ == "__main__":
+    for line in sys.stdin:
+        cr = line.split(',')[2]
+        sys.stdout.write("{}\t1\n".format(cr))
