3b7cdc87c2d4777cc430146c4923e4e098351b0b
Sol Beatriz Rico C <soricoc@unal.edu.co>
2020-02-12 18:41:17 -0500

    Tarea 4

--- 01-hadoop-50/q04-10/mapper.py
+++ 01-hadoop-50/q04-10/mapper.py
@@ -1,5 +1,7 @@
-import sys
-#
-#  >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+#! /usr/bin/env python
 
+import sys
+if __name__ == "__main__":
+    for line in sys.stdin:
+        cr = line.split()[0]
+        sys.stdout.write("{}\t1\n".format(cr))
