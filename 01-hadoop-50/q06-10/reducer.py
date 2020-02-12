44fbaa2db725498d1b126151ee9fa5a913d92f19
Sol Beatriz Rico C <soricoc@unal.edu.co>
2020-02-12 18:43:27 -0500

    Tarea 6

--- 01-hadoop-50/q06-10/reducer.py
+++ 01-hadoop-50/q06-10/reducer.py
@@ -1,4 +1,31 @@
+#!/usr/bin/env python
+
 import sys
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+
+if __name__ == '__main__':
+
+    curkey = None
+    mx = None
+    mn = None
+    
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = float(val)
+        
+        if mx is None:
+            mx = val
+            
+        if mn is None:
+            mn = val
+
+        if key == curkey:
+            mx = max(val, mx)
+            mn = min(val, mn)
+        else:
+            if curkey is not None:
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, mx, mn))
+            curkey = key
+            mx = val
+            mn = val
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, mx, mn))
