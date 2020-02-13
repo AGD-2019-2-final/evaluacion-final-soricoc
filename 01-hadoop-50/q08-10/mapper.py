e0bf6f5af54c76120acb280a71b123af6f4f75f9
Sol Beatriz Rico C <soricoc@unal.edu.co>
2020-02-12 19:47:49 -0500

    Tarea 8

--- 01-hadoop-50/q08-10/mapper.py
+++ 01-hadoop-50/q08-10/mapper.py
@@ -1,5 +1,8 @@
-import sys
-#
-#  >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+#! /usr/bin/env python
 
+import sys
+if __name__ == "__main__":
+    for line in sys.stdin:
+        l = line.split()[0]
+        c = line.split()[2]
+        sys.stdout.write("{}\t{}\n".format(l, c))
--- 01-hadoop-50/q08-10/reducer.py
+++ 01-hadoop-50/q08-10/reducer.py
@@ -1,4 +1,34 @@
+#!/usr/bin/env python
+
 import sys
-#
-#  >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+
+if __name__ == '__main__':
+
+    curkey = None
+    sum = None
+    avg = None
+    count = 1
+    
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = float(val)
+                
+        if sum is None:
+            sum = 0
+            
+        if avg is None:
+            avg = 0
+
+        if key == curkey:
+            sum = sum + val
+            count = count + 1
+            avg = sum / count            
+        else:
+            if curkey is not None:
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, sum, avg))
+            curkey = key
+            sum = val
+            avg = val
+            count = 1
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, sum, avg))
