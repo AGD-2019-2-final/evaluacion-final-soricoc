c2d1be253957b908c826efdecd6b87c60f5c086d
Sol Beatriz Rico C <soricoc@unal.edu.co>
2020-02-15 10:06:55 -0500

    Tarea 7

--- 01-hadoop-50/q07-10/mapper.py
+++ 01-hadoop-50/q07-10/mapper.py
@@ -1,4 +1,6 @@
+#! /usr/bin/env python
+
 import sys
-#
-#  >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+if __name__ == "__main__":
+    for line in sys.stdin:
+        sys.stdout.write("{}".format(line))
--- 01-hadoop-50/q07-10/reducer.py
+++ 01-hadoop-50/q07-10/reducer.py
@@ -1,4 +1,10 @@
+#!/usr/bin/env python
+
 import sys
-#
-#  >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+
+if __name__ == '__main__':   
+    lines = sys.stdin.readlines()
+    lines.sort(key=lambda x:int(x.split()[-1]))
+    lines.sort(key=lambda x:(x.split()[0]))  
+    for line in lines:
+         print(line),
